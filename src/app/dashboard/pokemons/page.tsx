2e78d06f783ff59a297d2524937fd1af0f1b52bd
Florencia Luz Duarte <florluzduarte@gmail.com>
2024-01-11 15:00:26 -0300

    wip: Pokemons integration, data fetching, PokemonsGrid, PokemonsCard

---
+++ src/app/dashboard/pokemons/page.tsx
@@ -0,0 +1,31 @@
+import { PokemonsGrid, PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+
+const getPokemons = async (
+  limit = 20,
+  offset = 0
+): Promise<SimplePokemon[]> => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  );
+
+  const data: PokemonsResponse = await response.json();
+
+  const pokemons = data.results.map((pokemon) => ({
+    id: pokemon.url.split("/").at(-2)!,
+    name: pokemon.name,
+  }));
+  return pokemons;
+};
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons(151);
+
+  return (
+    <div className="flex flex-col">
+      <span className="text-xl5 my-2">
+        Listado de Pokemons <small>estático</small>
+      </span>
+      <PokemonsGrid pokemons={pokemons} />
+    </div>
+  );
+}
---
+++ src/app/pokemons/index.ts
@@ -0,0 +1,5 @@
+export type { PokemonsResponse } from "./interfaces/pokemons-response";
+export type { SimplePokemon } from "./interfaces/simple-pokemon";
+
+export { PokemonsGrid } from "./components/PokemonsGrid";
+export { PokemonsCard } from "./components/PokemonsCard";
