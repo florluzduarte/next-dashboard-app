ef63f06bf05fa6cc03a8fd16aababae9b5931ed1
Florencia Luz Duarte <florluzduarte@gmail.com>
2024-01-12 20:12:16 -0300

    wip: creating dynamic routes for pokemons

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,7 +1,25 @@
-export default function PokemonPage() {
+import { Pokemon } from "@/pokemons";
+
+interface Props {
+  params: { id: string };
+}
+
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: "force-cache",
+  });
+  const data = await response.json();
+  console.log("Se cargó: ", data.name);
+  return data;
+};
+
+export default async function PokemonPage({ params }: Props) {
+  const pokemon = await getPokemon(params.id);
+
   return (
     <div>
-      <h1>Hello Pokemon Page</h1>
+      <h1>Hello Pokemon {params.id}</h1>
+      <div>{JSON.stringify(pokemon)}</div>
     </div>
   );
 }
--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -1,5 +1,7 @@
 export type { PokemonsResponse } from "./interfaces/pokemons-response";
 export type { SimplePokemon } from "./interfaces/simple-pokemon";
+export type { Pokemon } from "./interfaces/pokemon";
+
 
 export { PokemonsGrid } from "./components/PokemonsGrid";
 export { PokemonsCard } from "./components/PokemonsCard";
